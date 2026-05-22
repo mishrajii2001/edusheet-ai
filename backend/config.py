dd7a3385d5d85714ddb2f582eb0e2ad9cc5d0ca2
mishrajii2001 <amandeepmishra1234@gmail.com>
2026-05-23 03:25:40 +0530

    add config.py and env setup

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+APP_NAME = "EduSheet AI"
+VERSION = "1.0.0"
